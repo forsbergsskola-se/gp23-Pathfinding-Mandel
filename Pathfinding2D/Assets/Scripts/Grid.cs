0ec299a36bac14efda29703a7952fe4eb0cc4941
mandel <mandel.cohen@student.forsbergsskola.se>
2024-05-20 14:46:16 +0200

    Add bool[] and helper function

--- Pathfinding2D/Assets/Scripts/Grid.cs
+++ Pathfinding2D/Assets/Scripts/Grid.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    private bool[] walkableGrid = new bool[100];
+    public int width;
+
+    public bool IsWalkable(int x, int y)
+    {
+        return walkableGrid[y * width + x]; // find the right index in the array.
+    }
+    
+    
     // Start is called before the first frame update
     void Start()
     {

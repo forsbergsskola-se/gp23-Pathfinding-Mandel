4213866a53c773570535327e183c1fd446730506
mandel <mandel.cohen@student.forsbergsskola.se>
2024-05-21 12:01:54 +0200

    Try implementing GetCellForPosition

--- Pathfinding2D/Assets/Scripts/Grid.cs
+++ Pathfinding2D/Assets/Scripts/Grid.cs
@@ -12,4 +12,39 @@ public class Grid : MonoBehaviour
     {
         return walkableGrid[y * width + x].isWalkable; // find the right index in the array.
     }
+
+    public GridCell GetCellForPosition(Vector3 pos)
+    {
+        var maxX = width - 1;
+        var maxY = walkableGrid.Length / width - 1;
+
+        if (pos.x < 0 -0.5 || pos.x > maxX +0.5 || pos.y < 0-0.5 || pos.x > maxY +0.5)
+        {
+            throw new PositionOutsideOfGridExeption();
+        }
+
+        int i = 0;
+        var gridPos = new Vector3(0, 0, -1);
+        for (var y = 0; y < maxY; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                if (pos.x > gridPos.x -0.5 && pos.x <= gridPos.x + 0.5)
+                {
+                    if (pos.y > gridPos.y -0.5 && pos.y <= gridPos.y + 0.5)
+                    {
+                        return walkableGrid[i];
+                    }
+                }
+                i++;
+            }
+        }
+
+        return null;
+    }
+}
+
+public class PositionOutsideOfGridExeption : Exception
+{
+    public override string Message => "The position is outside of the grid!";
 }
--- Pathfinding2D/Assets/Scripts/PlayerController.cs
+++ Pathfinding2D/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Gold;
+    
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Grid grid = FindObjectOfType<Grid>();
+            GridCell start = grid.GetCellForPosition(this.transform.position);
+            GridCell end = grid.GetCellForPosition(Gold.transform.position);
+            var path = FindPath(grid, start, end);
+            // start courotine
+            // traverse the path
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    static IEnumerable<GridCell> FindPath(Grid grid, GridCell start, GridCell end)
     {
+        // Track visited cells
+        // track sells that need to be visited
+        // track the goal
+        // decide what cells to visit next
         
+        // if current == goal
+        // reconstruct path
+
+        throw new NotImplementedException();
+
     }
 }
